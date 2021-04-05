55c10717bf903795e5919c7f68986de1b4c2232d
quamainclark <quamainclark1@gmail.com>
2021-04-04 21:19:27 -0400

    create react app & defined router

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+// import PersonalInfoPage from './containers/PersonalInfo';
+// import ChatRoomPage from './containers/ChatRoom';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          {/* <Redirect to='/personalInfo' /> */}
+        </Route>
+        {/* <Route exact path='/personalInfo' component={PersonalInfoPage} />
+        <Route exact path='/chatRoom' component={ChatRoomPage} /> */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
