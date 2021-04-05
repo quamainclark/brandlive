6485e33b9842f3ee85770264938b60408993e85a
quamainclark <quamainclark1@gmail.com>
2021-04-04 21:23:46 -0400

    added reducer

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+export const initialState = {
+    // view: "home",
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        // case "SET_VIEW":
+        //     return {
+        //         ...state,
+        //         view: action.view,
+        //     };
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
